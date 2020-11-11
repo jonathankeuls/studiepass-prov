9c0a3e5699eb67bb451bacc18e020d626f907a69
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-11-11 13:34:10 +0100

    Uppgift 4

--- 4/script.js
+++ 4/script.js
@@ -1,9 +1,14 @@
 // 1. Skapa en variabel som innehåller en array med fyra strängar, t.ex. fyra namn på personer.
+const names = ['Jonathan', 'Anders', 'Tomas', 'Jack']
 
 // 2. Logga ut arrayen.
+console.log(names)
 
 // 3. Byt ut det andra elementet till något annat.
+names[1] = 'Bertil'
 
 // 4. Logga ut arrayen igen.
+console.log(names)
 
 // 5. Logga ut det sista elementet i arrayen.
+console.log(names[3])
